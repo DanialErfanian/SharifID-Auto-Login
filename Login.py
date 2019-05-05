d1b14a04dcfd69ed9978c56eeae101e7a4221c47
DanialErfanian <erfaniiyanerfan@gmail.com>
2019-05-05 09:48:41 +0430

    add login script

---
+++ Login.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+username = "danial.erfanian"
+password = "enter your password :)"
+
+driver = webdriver.Chrome("/home/danial/PycharmProjects/SharifID-Auto-Login/chromedriver")
+
+try:
+    driver.get("https://net2.sharif.edu/login")
+    driver.find_element_by_name("username").send_keys(username)
+    driver.find_element_by_name("password").send_keys(password)
+    driver.find_element_by_class_name("login100-form-btn").click()
+except:
+    pass
+if driver is not None:
+    driver.quit()
